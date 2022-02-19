3ff72d9d6bdab59e7816c6c442a416d088160557
Pruthviraj <pbhrashkal985@gmail.com>
2022-02-19 14:17:57 +0530

    Add files via upload

---
+++ 10_Array/4.py
@@ -0,0 +1,6 @@
+from array import *
+Stu_roll = array('i', [501,502,503,504,505])
+
+N = len(Stu_roll)
+for i in range(N):
+    print(Stu_roll[i])
---
+++ 10_Array/5.py
@@ -0,0 +1,7 @@
+from array import *
+Stu_roll = array('i', [501,502,503,504,505])
+
+N = len(Stu_roll)
+
+for i in range(N):
+    print(i, "=", Stu_roll[i])
