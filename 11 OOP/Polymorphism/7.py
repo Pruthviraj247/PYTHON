90d919719349bdb274f028abbb5b4fd9a3a19a55
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-01 09:29:44 +0530

    Create 7.py

---
+++ 11 OOP/Polymorphism/7.py
@@ -0,0 +1,31 @@
+class btech():
+    def barch(self):
+        print("Your Brach Is CS")
+    def fess(self):
+        print("Your Total fess 5lkh")
+    def side(self):
+        print("Your Collage Is IIT Mumbai")
+
+class Bsc():
+    def barch(self):
+        print("Your Brach Is IT")
+    def fess(self):
+        print("Your Total fess 40k")
+    def side(self):
+        print("Your Collage Is Govt collage of Solapur")
+
+X = btech()
+X.barch()
+
+Y=Bsc()
+Y.barch()
+
+def I_Need_collage(btech):
+    btech.fess()
+
+I_Need_collage(X)
+
+# def I_Need_collage(Bsc):
+#     btech.fess()
+#
+# I_Need_collage(Y)
