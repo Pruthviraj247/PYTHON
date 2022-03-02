2d4b2fd005fbb8cbbea8cd2663024f545404206d
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-02 17:38:41 +0530

    Create 1.py

---
+++ 11 OOP/Encapsulation/1.py
@@ -0,0 +1,14 @@
+class uni:
+    def btech(self,sub):
+        print("Your Btech Subject Is:", sub)
+
+    def fees(self,fee):
+        print("Your fess Are:",fee)
+
+UNI = uni()
+UNI.btech("CS")
+UNI.fees(500000)
+UNI.btech("IT")
+UNI.fees(25000)
+
+
