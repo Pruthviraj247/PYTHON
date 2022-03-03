f14fc91c1850f7f695a29428f1006e19b6527149
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-03 09:50:06 +0530

    Create 2.py

---
+++ 6 Taple/2.py
@@ -0,0 +1,11 @@
+A = (3,  1, 5, 3, 2, 7, 3, 4, 8, 3, 6,"ggg")
+
+B = (12, 15, 19, 11, 14, 17, 13)
+
+# print(A.index(6))
+# print(A.count(3))
+# print(9 in A)
+# print(9 not in A)
+# print(A+B)
+print(A<B)
+print(A>B)
