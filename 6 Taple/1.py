0a49c949ea6eea6bb867ef95844ce26b979d5d0a
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-03 09:49:40 +0530

    Create 1.py

---
+++ 6 Taple/1.py
@@ -0,0 +1,29 @@
+A = (1.7,2,4,5,8,4,7,5,3,6,9)
+
+B = ('Hel         lo      Hiii      Hi', 'hello', 'Hi', 'hi','hh')
+
+C = ('', '',  '', '', 0, '')
+
+D = ('', '', 0, '', '', '0', '')
+
+E = ('A', 'B', 'C', 'D', '0', '')
+
+# print(min(A),"\n")
+# print(min(B),"\n")
+# print(max(A),"\n")
+# print(max(B),"\n")
+# print(len(A),"\n")
+# print(len(B),"\n")
+# print(sum(A),"\n")
+# print(any(C),"\n")
+# print(any(E),'\n')
+# print(all(D),'\n')
+# print(all(E),'\n')
+print(sorted(A),"\n")
+print(id(A))
+
+
+
+
+
+
