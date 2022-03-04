18b192998362104a8e457f1e206f28a01a662e62
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-04 08:54:58 +0530

    Create 7.py

---
+++ 6 Taple/7.py
@@ -0,0 +1,7 @@
+Num = int(input("Enter A Number"))
+Tap = (22,1,2,4,9,99,88,55,33,45,3,23,65,69,1,29)
+
+if Num in Tap:
+    print("Yes Your Number In Tuple")
+else:
+    print("Sorry Your Number In Not A Tuple")
