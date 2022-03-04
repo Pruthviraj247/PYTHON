518745a13c8e77beba13ccdb53c481dc8e3bdb45
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-04 08:55:43 +0530

    Create 8.py

---
+++ 6 Taple/8.py
@@ -0,0 +1,9 @@
+Tap = (("","Pruthvi",12,55),("","Sara",24,66),("","Elisa",6,9),("","Jennie",30,88),("","Rose",2,44))
+# Name = input("Enter A Name")
+for u,o in Tap:
+    print(u,"Eting Pizza",o)
+
+
+
+
+
