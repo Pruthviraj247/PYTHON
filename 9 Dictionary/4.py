de7bd804420cb548d5ac03e1b64c693552948142
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-05 09:35:55 +0530

    Create 4.py

---
+++ 9 Dictionary/4.py
@@ -0,0 +1,8 @@
+D1 = {"Pruthvi":{"B":"Fruit","L":"Dal Roti","D":"Chicken"},
+      "Sara":{"B":"Juice","L":"Fish","D":"Chicken"},
+      "Jennie":{"B":"Salad","L":"Rice Balls","D":"Sushi"}}
+
+
+print(D1["Pruthvi"]["B"])
+print(D1["Sara"]["L"])
+print(D1["Jennie"]["D"])
