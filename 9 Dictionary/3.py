86f1568984c1638faed13d29f650ee6736ef3694
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-05 09:35:16 +0530

    Create 3.py

---
+++ 9 Dictionary/3.py
@@ -0,0 +1,4 @@
+D1 = {"Pruthvi":{"B":"Fruit","L":"Dal Roti","D":"Chicken"},
+      "Sara":{"B":"Juice","L":"Fish","D":"Chicken"},
+      "Jennie":{"B":"Salad","L":"Rice Balls","D":"Sushi"}}
+print(D1["Sara"])
