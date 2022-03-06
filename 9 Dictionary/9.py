12f3e86dcd5244087991db807a9f933be61fe44b
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-06 09:52:06 +0530

    Create 9.py

---
+++ 9 Dictionary/9.py
@@ -0,0 +1,16 @@
+D1 = {"Pruthvi":"Roti","Sara":"Burger","Jennie":"Pizza",15:"Aditi"}
+
+# print(D1)
+# print(D1.items())
+# D1.pop("Pruthvi")
+# print(D1)
+# print(D1.pop("Pruthvi"))
+# print(len(D1))
+# print(type(D1))
+# print(D1.popitem())
+# D1.popitem()
+# print(D1)
+# D1.clear()
+# print(D1)
+print(15 in D1)
+print(15 not in D1)
