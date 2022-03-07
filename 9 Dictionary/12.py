5c30a3a2b2d6e48862a3e1006af659545f4b3b5a
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-07 08:59:08 +0530

    Create 12.py

---
+++ 9 Dictionary/12.py
@@ -0,0 +1,9 @@
+D1 = {}
+
+D1[0] = "Pruthvi "
+D1[1] = "Sara"" Roti "
+D1[2] = "Jennie"" L ""Rice"
+D1[3] = "Rose"" B "" Juice "" L "" Fish "
+D1[4] = "Lisa "" B "" Salad "" L "" Sushi "" D "" Chicken"
+
+print(D1)
