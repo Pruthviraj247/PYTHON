15a7c4a05adf724a5bab1b1d207dcce687e545b0
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-09 09:01:57 +0530

    Create 19.py

---
+++ 9 Dictionary/19.py
@@ -0,0 +1,18 @@
+D1 = {"Pruthvi":"Chicken",
+      "Sara":"Pizza",
+      "Jennie":"Burger",
+      "Lisa":"junk Food"
+}
+
+X1 = input("Enter Your Name")
+
+if X1 not in D1:
+      print("Sorry you name not in list")
+else:
+      print(D1[X1])
+
+
+
+
+
+
