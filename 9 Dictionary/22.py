53126f3919136ab95a89b20895e9df0f0385c7da
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-09 09:03:36 +0530

    Create 22.py

---
+++ 9 Dictionary/22.py
@@ -0,0 +1,8 @@
+while (True):
+    inp = int(input("Enter A Number"))
+    if inp < 100:
+        print("Congress You Number Is Creerat")
+        break
+    else:
+        print("Sorry Your No Is Not Correat")
+        continue
