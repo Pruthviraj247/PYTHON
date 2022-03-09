c8dd484fcebff4f7db8ddc544d480bdc5d215470
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-09 09:04:08 +0530

    Create 23.py

---
+++ 9 Dictionary/23.py
@@ -0,0 +1,14 @@
+while(True):
+    Num1=int(input("Enter A Number"))
+    if Num1<20:
+        print("You Entered Number Is Too Low")
+    elif Num1<24:
+        print("All Most There")
+    elif Num1<26:
+        print("All Most There")
+    elif Num1<50:
+        print("You Entered Number Is Too High")
+        break
+    else:
+        print("Your Number Is Uncreeat")
+        continue
