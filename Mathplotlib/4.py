9f81ed6bf68b2acf91be76726b399d5d666af55a
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-10 08:41:56 +0530

    Create 4.py

---
+++ Mathplotlib/4.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+W = [1,5,6,9,10]
+X = [4,6,4,5,6]
+Y = [8,5,1,4,4]
+Z = [6,9,7,4,1]
+
+plt.plot(W,X,label="Line 1")
+plt.plot(Y,Z,label="Line 2")
+plt.title("This Is A Title")
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
