be351e45d256836e642ae61ac3bfd9752f76eb29
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-14 08:56:53 +0530

    Create 4.py

---
+++ List/4.py
@@ -0,0 +1,18 @@
+X = [5, 9, 3, 7, 1, 6, 8,66]
+Y = []
+X.append(69)
+X.append(96)
+Y.append(12)
+Y.append(24)
+Y.append(48)
+print(X)
+print(Y)
+X.insert(2,45)
+X.insert(5,77)
+print(X)
+X.remove(8)
+print(X)
+X.pop()
+print(X)
+X[1]=85
+print(X)
