278e05c7dd757ec3f2e15b53a75bf47cfffad5f1
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-14 08:56:30 +0530

    Create 3.py

---
+++ List/3.py
@@ -0,0 +1,14 @@
+X = [9, 1, 7, 2, 4, 8, 3, 5, 6, 15, 11, 19, 20]
+# print(X[:])
+# print(X[2:])
+# print(X[:5])
+# print(X[1:5])
+# print(X[::2])
+# print(X[1:10:2])
+# print(X[::-2])
+# Y = [1]
+# print(Y)
+# Z = (5,)
+# A = (55)
+# print(A)
+# print(Z)
