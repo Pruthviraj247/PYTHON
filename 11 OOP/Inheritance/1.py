e77d790258bf8f2bbafa11af99b24cf1837dba11
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-16 06:47:08 +0530

    Create 1.py

---
+++ 11 OOP/Inheritance/1.py
@@ -0,0 +1,11 @@
+
+
+class A:
+    """This Is Program of Single Inheritance"""
+    def aofa(self):
+        print("This Is A 1st Inheritance")
+        print("This Is A Over 1st Class Also")
+
+B=A()
+B.AofA()
+print(A.__doc__)
