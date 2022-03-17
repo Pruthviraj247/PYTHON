10510ef7112d5ca632fefba8e4961dcac7e919f0
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-17 09:47:47 +0530

    Create 5.py

---
+++ 11 OOP/Inheritance/5.py
@@ -0,0 +1,16 @@
+"""This Is Program of Hierarchical Inheritance"""
+class laptop():
+    def ram(self):
+        self.ram()
+    def processor(self):
+        self.processor()
+    def storage(self):
+        self.storage
+    def type(self):
+        print("This Is A Laptop")
+
+
+# laptop(8,5,1)
+# Z = laptop(8,5,1)
+# Z.ram()
+print(dir(laptop))
