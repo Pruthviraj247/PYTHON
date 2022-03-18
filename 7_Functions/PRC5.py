f70ec56abbf4ec069738009c2a4eb8e836d0e95b
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-18 07:29:32 +0530

    Create PRC5.py

---
+++ 7_Functions/PRC5.py
@@ -0,0 +1,40 @@
+def fun():
+    """
+
+    :rtype: object
+    """
+    N = int(input("Enter A Number"))
+    if N<20:
+        print("Hi How Are You")
+        if N<15:
+            print("Hi How Are You")
+            if N<10:
+                print("Hi How Are You")
+                if N<5:
+                    print("Hi How Are You")
+                    if N<4:
+                        print("Hi How Are You")
+                        if N<3:
+                            print("Hi How Are You")
+                            if N<2:
+                                print("Hi How Are You")
+                                if N<1:
+                                    print("Hi How Are You")
+                                else:
+                                    pass
+                            else:
+                                pass
+                        else:
+                            pass
+                    else:
+                        pass
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
+
+    else:
+        print("Sorry")
+fun()
