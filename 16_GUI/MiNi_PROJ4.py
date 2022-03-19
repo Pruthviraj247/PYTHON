76b58b76c14e8265f5bd0e0f89d8b0dfbcd4b58d
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-19 15:25:41 +0530

    Create MiNi_PROJ4.py

---
+++ 16_GUI/MiNi_PROJ4.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("600x600")
+root.title("Welcome To Domino's")
+root.configure(bg="aliceblue")
+
+
+def sub():
+    L2.config(text=clicked.get(), bg="aliceblue", fg="green", font=("arial", 10, "bold"))
+
+
+L = Label(text="Welcome To Domino's", bg="aliceblue", fg="red", font=("arial", 15, "bold"))
+L.place(x=200, y=5)
+
+Pizza = [
+    "Choice Your Pizza",
+    "The Unthinkable Pizza Price: 499",
+    "Veg Extravaganza Price: 299",
+    "Mexican Green Wave Price: 899",
+    "Chicken Dominator Price: 599",
+    "Chicken Pepperoll Price: 459",
+    "Chicken Flesta Price:299"
+]
+
+clicked = StringVar()
+clicked.set("Choice Your Pizza")
+
+Drop = OptionMenu(root, clicked, *Pizza)
+Drop.place(x=230, y=50)
+
+Ch = Checkbutton(root, text="Do You Want Cocola", bg="aliceblue", fg="red", font=("arial", 9, "bold"))
+Ch.place(x=230, y=230)
+
+Submit = Button(root, text="Order", command=sub,fg="red",font=("arial", 10, "bold"))
+Submit.place(x=260, y=250)
+
+L2 = Label(root, text=" ")
+L2.place(x=200, y=300)
+
+root.mainloop()
