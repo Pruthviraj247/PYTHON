659c86cc2ac32f7d016a0a57896d81673cbf6985
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-20 09:47:16 +0530

    Create 4.py

---
+++ 16_GUI/DAY 2/4.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+root=Tk()
+
+root.geometry("700x500")
+
+root.title("Hey This Is Title")
+
+L1 = Label(text="Hey This Is A Label")
+L1.place(x=15,y=20)
+
+L2 = Label(text="Hey This Is A Label")
+L2.place(x=30,y=60)
+
+L3 = Label(text="Hey This Is A Label")
+L3.place(x=60,y=100)
+
+L4 = Label(text="Hey This Is A Label")
+L4.place(x=90,y=140)
+
+L5 = Label(text="Hey This Is A Label")
+L5.place(x=120,y=180)
+
+L6 = Label(text="Hey This Is A Label")
+L6.place(x=410,y=20)
+
+L7 = Label(text="Hey This Is A Label")
+L7.place(x=370,y=60)
+
+L8 = Label(text="Hey This Is A Label")
+L8.place(x=330,y=100)
+
+L9 = Label(text="Hey This Is A Label")
+L9.place(x=290,y=140)
+
+L10 = Label(text="Hey This Is A Label")
+L10.place(x=250,y=180)
+
+
+root.mainloop()
