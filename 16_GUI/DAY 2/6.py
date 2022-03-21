808017f5d7f4be30433202a36e0f011c2ef3f8df
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-21 09:16:33 +0530

    Create 6.py

---
+++ 16_GUI/DAY 2/6.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root=Tk()
+
+root.geometry("250x250")
+
+root.title("Hey This Is Title")
+
+L1 = Label(root, text="Hey Welcome Come To Label", bg="red")
+L1.pack(side=TOP,fill=X)
+
+root.mainloop()
