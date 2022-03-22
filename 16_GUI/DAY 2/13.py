fd7ebdd84a397b4e752289f83002a44f8d6df4bb
Pruthviraj <pbhrashkal985@gmail.com>
2022-03-22 09:34:56 +0530

    Create 13.py

---
+++ 16_GUI/DAY 2/13.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root=Tk()
+
+root.geometry("500x500")
+
+root.title("Hey This Is Title")
+
+sp = Spinbox(root, from_=0, to=20)
+sp.pack()
+
+root.mainloop()
+root.mainloop()
